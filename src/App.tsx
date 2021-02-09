8a026af07abc3f11183596adc71ddee99e6716d9
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2021-02-09 20:57:59 +0000

    Fix game page route

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import "@ionic/react/css/display.css";
 import "./theme/variables.css";
 import Login from "./pages/Login";
 import AuthVerify from "./pages/AuthVerify";
+import Game from "./pages/Game";
 
 const App: React.FC = () => {
   const [isAuthed, setIsAuthed] = React.useState(false);
@@ -34,6 +35,7 @@ const App: React.FC = () => {
         <IonRouterOutlet animated={true}>
           <Redirect exact path="/" to="/home" />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/game" component={Game} />
           <Route
             exact
             path="/login"
